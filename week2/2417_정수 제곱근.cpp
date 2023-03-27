38bce370ab594206b0e226c68aafe8c27ec62b19
Iron-Hee <maks012199@naver.com>
2023-03-28 01:02:28 +0900

    2417 정수 제곱근 코드 수정

--- week2/2417_정수 제곱근.cpp
+++ week2/2417_정수 제곱근.cpp
@@ -8,28 +8,21 @@ int main(void) {
 	cin.tie(nullptr);
 	long long n;
 	cin >> n;
-	long long low = 0, high = LLONG_MAX;
+	long long low = 0, high = pow(2, 32);
+	long long ans = 0;
 	while (low + 1 < high) {
 		long long mid = (low + high) / 2;
 		if (pow(mid, 2) >= n) {			// F|T(mid)인지 확인
-			if (pow(mid - 1, 2) >= n) {
-				high = mid;
-			}
-			else {
-				cout << mid;
-				return 0;
-			}
+			high = mid;
+			ans = mid;
 		}
 		else {							// F(mid)|T인지 확인
-			if (pow(mid + 1, 2) >= n) {
-				cout << mid + 1;
-				return 0;
-			}
-			else {
-				low = mid;
-			}
+			low = mid;
 		}
 	}
-	cout << 0;
+	if (n == 0) {
+		ans = 0;
+	}
+	cout << ans;
 	return 0;
 }
